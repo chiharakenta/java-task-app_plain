cd4552eb395313546f27136740a382f9be9087a9
chiharakenta <chiharakenta0519@gmail.com>
2025-11-10 12:42:14 +0900

    refactor: コンパイルコマンドを見やすく

--- compile.sh
+++ compile.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
-# クリーン
+# --------------------------
+# ターゲットフォルダの中身をクリア
 rm -rf target/*
-mkdir -p target/WEB-INF/classes
+# --------------------------
 
-# javaのコンパイル
+# --------------------------
+# コンパイル
+## javaのコンパイル
 find src/java -name "*.java" > sources.txt
 javac -d target/WEB-INF/classes -cp "lib/*:src/webapp/WEB-INF/lib/*" @sources.txt
 rm sources.txt
 
-# webappフォルダ内のコピー
-cp -r src/webapp/* target
+## java以外のファイルのコピー
+cp -r src/webapp/* target/
+# --------------------------
 
-# デプロイ
+
+# --------------------------
+# Tomcatにデプロイ
 APP_PATH="$CATALINA_HOME/webapps/ROOT"
 rm -rf -- $APP_PATH
 mkdir $APP_PATH
 cp -r target/* $APP_PATH
+# --------------------------
 
+
+# --------------------------
 # tomcatの再起動
 $CATALINA_HOME/bin/shutdown.sh
 $CATALINA_HOME/bin/startup.sh
+# --------------------------
